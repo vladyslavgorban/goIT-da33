e4c3d26ca1eca64d6916ad167dca8a6b165229e0
vladyslavgorban <vladislav.gorban@gmail.com>
2026-02-25 18:58:33 +0100

    add age input feature

--- git_test.py
+++ git_test.py
@@ -3,4 +3,7 @@ name = input("Enter your name: ")
 print(f'Hello {name}')
 
 height = input("Enter your height: ")
-print(f'Your height is {height}')
+print(f'Your height is {height} meters')
+
+age = input("Enter your age: ")
+print(f'Your age is {age} years')
